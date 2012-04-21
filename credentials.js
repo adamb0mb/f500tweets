01f13c361226b9ab21065e70d245fd1ee5a793c1
Joe Cartano <joe@psmacbook-001.local>
2012-04-21 16:05:15 -0700

    adding simple twitter streamer

---
+++ credentials.js
@@ -0,0 +1,8 @@
+var credentials = {
+    consumer_key: 'bqwOHc3lWLDfbO1o56zXPw',
+    consumer_secret: 'ad2NpZBwMSp968VOfZPwNuTwsbLWOXfFyiDuqSLWORs',
+    access_token_key: '44461409-vbNnWJhqX02ZUDipTu1zs9ZkcliD27KNWiqovBvOi',
+    access_token_secret: 'IUqaGMXUhCfRJVEIINBkgVwNIMuptzjPMSqw9GKQ3ww'
+};
+
+module.exports = credentials;
